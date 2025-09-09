788b946d13a6f17ce9aba3e89bd82ed4cccda1d6
rologos <170283129+rologos@users.noreply.github.com>
2025-09-09 12:53:08 -0500

    add path via sys arguments

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    path="./books/frankenstein.txt"
-
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    count_words(get_book_text(path))
-    print("--------- Character Count -------")
-    sort_dict(count_characters(get_book_text(path)))
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+    else:
+        path = sys.argv[1]
+        print("============ BOOKBOT ============")
+        print("Analyzing book found at books/frankenstein.txt...")
+        print("----------- Word Count ----------")
+        count_words(get_book_text(path))
+        print("--------- Character Count -------")
+        sort_dict(count_characters(get_book_text(path)))
 main()
